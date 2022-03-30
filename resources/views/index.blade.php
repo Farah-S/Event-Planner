2e158f5821489aff10be6b4997d62f9b0a700f6b
Farah-S <farah.faroha132@gmail.com>
2022-03-31 01:52:03 +0200

    homepage edits

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,7 +10,7 @@
                     <div class="row">
                         <div class="col-xl-9">
                             <div class="title_text">
-                                <h3>Egypt's Best Online Event Planner</h3>
+                                <h3 class='slider'>Online Event Planning</h3>
                             </div>
                         </div>
                     </div>
@@ -22,28 +22,39 @@
 
     <!-- about_area_start -->
     <div class="about_area">
-        <div class="shape-1 d-none d-xl-block">
-            <img src="img/about/shap1.png" alt="">
-        </div>
-        <div class="shape-2 d-none d-xl-block">
-            <img src="img/about/shap2.png" alt="">
-        </div>
         <div class="container">
             <div class="row align-items-center">
                 <div class="col-xl-6 col-md-6">
                     <div class="about_thumb">
-                        <img src="img/about/about.png" alt="">
+                        <img src="img/about/white.jpg" alt="">
                     </div>
                 </div>
                 <div class="col-xl-5 offset-xl-1 col-md-6">
                     <div class="about_info">
                         <div class="section_title">
-                            <h3>The Biggest Design <br>
-                                Conference of the <br>
-                                Year 2019</h3>
+                            <h3>Event Planning</h3>
                         </div>
-                        <p>Our set he for firmament morning sixth subdue darkness creeping gathered divide our let god.
-                            moving. Moving in fourth air night bring upon you’re it beast.</p>
+                        <p>We offer many event types like conferences, corporate, graduation events and more</p>
+                        <a href="#" class="boxed-btn-red">Learn More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+     <div class="about_area">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-xl-6 col-md-6">
+                    <div class="about_thumb">
+                        <img src="img/about/deco.jpg" alt="">
+                    </div>
+                </div>
+                <div class="col-xl-5 offset-xl-1 col-md-6">
+                    <div class="about_info">
+                        <div class="section_title">
+                            <h3>Production House</h3>
+                        </div>
+                        <p>As well as events we offer decoration services</p>
                         <a href="#" class="boxed-btn-red">Learn More</a>
                     </div>
                 </div>
