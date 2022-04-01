b3906fc52fa8c7acb1f3399cd211ca1c18cf7680
IbrahimBaher <ibrahim1909541@miuegypt.edu.eg>
2022-04-01 14:30:15 +0200

    About page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,21 +4,24 @@
 
     <!-- slider_area_start -->
     <div class="slider_area slider_bg_1">
-        <div class="slider_text">
+    <div class="slider_text">
             <div class="container">
                 <div class="position_relv">
                     <div class="row">
-                        <div class="col-xl-8">
-                            <div class="title_text title_text2 ">
-                                <h3>Event Speakers</h3>
-                                <a href="#" class="boxed-btn-white">Add to your Calendar</a>
+                            <div class="col-xl-8col-lg-5">
+                                <div class="title_text title_text2 ">
+                                    <h3 class='slider'>About Us</h3>
+                                    <!--
+                                    <a href="#" class="boxed-btn-white">Add to your Calendar</a>
+                                    -->
+                                </div>
                             </div>
                         </div>
-                    </div>
                 </div>
             </div>
         </div>
-        <div class="countDOwn_area">
+        </div>
+        <!-- <div class="countDOwn_area">
             <div class="container">
                 <div class="row align-items-center">
                     <div class="col-xl-4 col-md-6 col-lg-4">
@@ -39,11 +42,26 @@
 
                 </div>
             </div>
-        </div>
+        </div> -->
     </div>
     <!-- slider_area_end -->
 
     <!-- speakers_start -->
+    <div class="container">
+            <div class="row align-items-center">
+                <div class="col-xl-8 col-lg-8">
+                <div class="section_title">
+                            <h3>About the Company</h3>
+                        </div>
+                </div>
+                <div class="col-xl-8 col-lg-8">
+                    <div class="about_info">
+                        <p>This is some info about the company </p>
+                     
+                    </div>
+                </div>
+            </div>
+        </div>
     <div class="speakers_area inc_padd">
         <div class="container">
             <div class="row">
@@ -61,7 +79,7 @@
                         </div>
                         <div class="speaker_name text-center">
                             <h3>Jonson Miller</h3>
-                            <p>Creative Director</p>
+                            <p>Ragel Gada3 we 3am 2el nas Ragel Gada3 we 3am 2el nas Ragel Gada3 we 3am 2el nas Ragel Gada3 we 3am 2el nas Ragel Gada3 we 3am 2el nas Ragel Gada3 we 3am 2el nas Ragel Gada3 we 3am 2el nas</p>
                         </div>
                     </div>
                     <div class="single_speaker">
@@ -82,7 +100,7 @@
                     </div>
                 </div>
                 <div class="col-xl-5 offset-xl-2 col-md-6">
-                    <div class="single_speaker">
+                    <!-- <div class="single_speaker">
                         <div class="speaker_thumb">
                             <img src="img/speakers/2.png" alt="">
                             <div class="hover_overlay">
@@ -97,7 +115,7 @@
                             <h3>Marked Macau</h3>
                             <p>UI/UX Designer</p>
                         </div>
-                    </div>
+                    </div> -->
                     <div class="single_speaker">
                         <div class="speaker_thumb">
                             <img src="img/speakers/1.png" alt="">
@@ -122,7 +140,7 @@
 
 
     <!-- resister_book_start -->
-    <div class="resister_book resister_bg_1">
+    <!-- <div class="resister_book resister_bg_1">
         <div class="container">
             <div class="row">
                 <div class="col-xl-12">
@@ -134,11 +152,11 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
     <!-- resister_book_end -->
 
     <!-- brand_area_start -->
-    <div class="brand_area">
+    <!-- <div class="brand_area">
         <div class="container">
             <div class="row">
                 <div class="col-xl-12">
@@ -165,7 +183,7 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
     <!-- brand_area_end -->
 
     <!-- faq_area_Start -->
@@ -175,7 +193,7 @@
                 <div class="col-xl-12">
                     <div class="serction_title_large mb-95">
                         <h3>
-                            Frequently Ask
+                            FAQ
                         </h3>
                     </div>
                 </div>
@@ -183,21 +201,19 @@
             <div class="row">
                 <div class="col-xl-12">
                     <div id="accordion">
-                        <div class="card">
-                            <div class="card-header" id="headingTwo">
+                    <div class="card">
+                            <div class="card-header" id="headingOne">
                                 <h5 class="mb-0">
-                                    <button class="btn btn-link collapsed" data-toggle="collapse"
-                                        data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-
-                                        <img src="img/barnd/info.png" alt=""> Is WordPress hosting worth it?
+                                    <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne"
+                                        aria-expanded="true" aria-controls="collapseOne">
+                                        <img src="img/barnd/info.png" alt="">Question1</span>
                                     </button>
                                 </h5>
                             </div>
-                            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion"
-                                style="">
+                            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne"
+                                data-parent="#accordion" style="">
                                 <div class="card-body">
-                                    Our set he for firmament morning sixth subdue darkness creeping gathered divide our
-                                    let god moving. Moving in fourth air night bring upon
+                                    Answer1
                                 </div>
                             </div>
                         </div>
@@ -206,55 +222,52 @@
                                 <h5 class="mb-0">
                                     <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne"
                                         aria-expanded="true" aria-controls="collapseOne">
-                                        <img src="img/barnd/info.png" alt="">What are the advantages <span>of WordPress
-                                            hosting over shared?</span>
+                                        <img src="img/barnd/info.png" alt="">Question2</span>
                                     </button>
                                 </h5>
                             </div>
                             <div id="collapseOne" class="collapse show" aria-labelledby="headingOne"
                                 data-parent="#accordion" style="">
                                 <div class="card-body">
-                                    Our set he for firmament morning sixth subdue darkness creeping gathered divide our
-                                    let god moving. Moving in fourth air night bring upon
+                                    Answer2
                                 </div>
                             </div>
                         </div>
                         <div class="card">
-                            <div class="card-header" id="headingThree">
+                            <div class="card-header" id="headingOne">
                                 <h5 class="mb-0">
-                                    <button class="btn btn-link collapsed" data-toggle="collapse"
-                                        data-target="#collapseThree" aria-expanded="false"
-                                        aria-controls="collapseThree">
-                                        <img src="img/barnd/info.png" alt=""> Where the Venue?
+                                    <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne"
+                                        aria-expanded="true" aria-controls="collapseOne">
+                                        <img src="img/barnd/info.png" alt="">Question3</span>
                                     </button>
                                 </h5>
                             </div>
-                            <div id="collapseThree" class="collapse" aria-labelledby="headingThree"
+                            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne"
                                 data-parent="#accordion" style="">
                                 <div class="card-body">
-                                    Our set he for firmament morning sixth subdue darkness creeping gathered divide our
-                                    let god moving. Moving in fourth air night bring upon
+                                    Answer3
                                 </div>
                             </div>
                         </div>
                         <div class="card">
-                            <div class="card-header" id="heading_4">
+                            <div class="card-header" id="headingOne">
                                 <h5 class="mb-0">
-                                    <button class="btn btn-link collapsed" data-toggle="collapse"
-                                        data-target="#collapse_4" aria-expanded="false" aria-controls="collapse_4">
-                                        <img src="img/barnd/info.png" alt=""> How can I attend <span>the Event from
-                                            Asia?</span>
+                                    <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne"
+                                        aria-expanded="true" aria-controls="collapseOne">
+                                        <img src="img/barnd/info.png" alt="">Question4</span>
                                     </button>
                                 </h5>
                             </div>
-                            <div id="collapse_4" class="collapse" aria-labelledby="heading_4" data-parent="#accordion"
-                                style="">
+                            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne"
+                                data-parent="#accordion" style="">
                                 <div class="card-body">
-                                    Our set he for firmament morning sixth subdue darkness creeping gathered divide our
-                                    let god moving. Moving in fourth air night bring upon
+                                    Answer4
                                 </div>
                             </div>
                         </div>
+                        
+                      
+                        
                     </div>
                 </div>
             </div>
