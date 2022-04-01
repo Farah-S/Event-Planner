49fdf845f32e3d4bca558a26cf2084e0917a5734
IbrahimBaher <ibrahim1909541@miuegypt.edu.eg>
2022-04-01 20:28:09 +0200

    profile page

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,79 @@
+@extends('layout')
+@section('title','Profile')
+@section('content')
+
+
+    <!-- slider_area_start -->
+    <div class="slider_area">
+    
+        </div>
+        <!-- <div class="countDOwn_area">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-xl-4 col-md-6 col-lg-4">
+                        <div class="single_date">
+                            <i class="ti-location-pin"></i>
+                            <span>City Hall, New York City</span>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-md-6 col-lg-3">
+                        <div class="single_date">
+                            <i class="ti-alarm-clock"></i>
+                            <span>12-15 Sep 2019</span>
+                        </div>
+                    </div>
+                    <div class="col-xl-5 col-md-12 col-lg-5">
+                        <span id="clock"></span>
+                    </div>
+
+                </div>
+            </div>
+        </div> -->
+   
+    <!-- slider_area_end -->
+
+    <!-- speakers_start -->
+    <div class="speakers_area inc_padd">
+        <div class="container">
+            <div class="row">
+                <div class="single_speaker">
+                        <div class="speaker_thumb">
+                            <img src="img/speakers/1.png" alt="">
+                            <div class="hover_overlay">
+                                <div class="social_icon">
+                                    <a href="#"><i class="fa fa-facebook"></i></a>
+                                    <a href="#"><i class="fa fa-instagram"></i></a>
+                                    <a href="#"><i class="fa fa-twitter"></i></a>
+                                </div>
+                                
+                            </div>
+                            </div>
+                
+                    <div class="about_info">
+                        <div class="section_title">
+                            <h3>Name</h3>
+                            <h4>Ibrahim Baher</h4>
+                        </div>
+                        
+                      
+                    </div>
+                    <div class="about_info">
+                        <div class="section_title">
+                            <h3>Email</h3>
+                            <h4>Ibrahim.baher@email.com</h4>
+                        </div>
+                        
+                      
+                    </div>
+                
+                        </div>
+                        
+                    </div>
+                    
+                </div>    
+                </div>  
+                </div>  
+                </div>  
+  
+
+@endsection
