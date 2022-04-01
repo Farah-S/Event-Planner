15ad371cf5b2165d815d593731c9314a2f434186
mohamedAyman <modyayman2001@gmail.com>
2022-04-01 18:26:07 +0200

    Signup-v1.2

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -10,7 +10,7 @@
                     </div>
                     <div class="col-lg-8">
                         <form class="form-contact contact_form" action="contact_process.php" method="post" id="contactForm" novalidate="novalidate">
-                            <div class="row">
+                            <div class="row row3">
                             <div class="col-sm-6">
                                     <div class="form-group">
                                         <input class="form-control valid" name="name" id="name" type="text" onfocus="this.placeholder = ''" onblur="this.placeholder = 'First Name'" placeholder="First Name">
@@ -33,40 +33,17 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="form-group mt-3">
+                            <div class="form-group mt-3 row2">
                                <!-- <input type="file" name="profilepic" class="form-control-file" > -->
                                 <label class="button button-contactForm boxed-btn custom-file-upload">
                                     <input type="file"/>
                                     <i class="fa fa-cloud-upload"></i> Upload Image
                                 </label>
                             </div>
-                            <div class="form-group mt-3">
+                            <div class="form-group mt-3 row2">
                                 <button type="submit" class="button button-contactForm boxed-btn sigup-button">Sign-Up</button>
                             </div>
                         </form>
-                    </div>
-                    <div class="col-lg-3 offset-lg-1">
-                        <div class="media contact-info">
-                            <span class="contact-info__icon"><i class="ti-home"></i></span>
-                            <div class="media-body">
-                                <h3>Buttonwood, California.</h3>
-                                <p>Rosemead, CA 91770</p>
-                            </div>
-                        </div>
-                        <div class="media contact-info">
-                            <span class="contact-info__icon"><i class="ti-tablet"></i></span>
-                            <div class="media-body">
-                                <h3>+1 253 565 2365</h3>
-                                <p>Mon to Fri 9am to 6pm</p>
-                            </div>
-                        </div>
-                        <div class="media contact-info">
-                            <span class="contact-info__icon"><i class="ti-email"></i></span>
-                            <div class="media-body">
-                                <h3>support@colorlib.com</h3>
-                                <p>Send us your query anytime!</p>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
