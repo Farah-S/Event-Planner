eab661ffa72aeb6e7b9e232df74282b6f1d1ea8a
ahmad123ezpz <ahmed.amr.mmm@gmail.com>
2022-04-01 16:28:43 +0200

    updates on login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,23 +3,23 @@
 @section('content')
 <section class="contact-section">
             <div class="container">
-                <div class="row">
-                    <div class="col-12">
-                        <h3 class=" slider slider2 slider3">Log In</h3>
+                <div class="row justify-content-center" >
+                    <div class="col-8 align-self-center">
+                        <h3 class=" slider slider2 slider3">Log In</h3><br>
                     </div>
                     <div class="col-lg-8">
                         <form class="form-contact contact_form" action="contact_process.php" method="post" id="contactForm" novalidate="novalidate">
-                            <div class="row">
+                            <div class="row justify-content-center">
                                 <div class="col-sm-6">
                                     <div class="form-group">
                                         <input class="form-control valid" name="email" id="email" type="email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter email address'" placeholder="Email">
                                     </div>
                                 </div>
                             </div>
-                            <div class="row">
+                            <div class="row justify-content-center">
                                 <div class="col-sm-6">
                                     <div class="form-group">
-                                        <input class="form-control valid" value="" id="password" input type="password" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter your Password'" placeholder="Password">
+                                        <input class="form-control valid" value="" id="password" input type="password" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter your Password'" placeholder="Password"><br>
                                         <input type="checkbox" onclick="myFunction()"> Show Password
                                     </div>
                                 </div>
@@ -28,29 +28,6 @@
                                 <button type="submit" class="button button-contactForm boxed-btn sigup-button">Log In</button>
                             </div>
                         </form>
-                    </div>
-                    <div class="col-lg-3 offset-lg-1">
-                        <div class="media contact-info">
-                            <span class="contact-info__icon"><i class="ti-home"></i></span>
-                            <div class="media-body">
-                                <h3>Buttonwood, California.</h3>
-                                <p>Rosemead, CA 91770</p>
-                            </div>
-                        </div>
-                        <div class="media contact-info">
-                            <span class="contact-info__icon"><i class="ti-tablet"></i></span>
-                            <div class="media-body">
-                                <h3>+1 253 565 2365</h3>
-                                <p>Mon to Fri 9am to 6pm</p>
-                            </div>
-                        </div>
-                        <div class="media contact-info">
-                            <span class="contact-info__icon"><i class="ti-email"></i></span>
-                            <div class="media-body">
-                                <h3>support@colorlib.com</h3>
-                                <p>Send us your query anytime!</p>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
