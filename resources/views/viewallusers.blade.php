799e703b15e4ed99557b2aa0a01d5a1858dd630b
ahmad123ezpz <ahmed.amr.mmm@gmail.com>
2022-04-03 01:10:44 +0200

    Created the List of all users page with a prototype of the table

---
+++ resources/views/viewallusers.blade.php
@@ -0,0 +1,114 @@
+@extends('layout')
+@section('title','View Users')
+@section('content')
+<section class="contact-section">
+    <div class="row justify-content-center">
+                        <div class="col-12">
+                            <h3 class=" slider slider2 align-mid">Users Table</h3>
+                        </div>
+                    </div>
+<div class="container">
+    <table class="table table-bordered table-dark table-hover">
+    <caption>List of users</caption>
+    <thead>
+        <tr>
+        <th scope="col">#</th>
+        <th scope="col">First Name</th>
+        <th scope="col">Last Name</th>
+        <th scope="col">User Type</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>Cleint</td>
+        </tr>
+        <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>Cleint</td>
+        </tr>
+        <tr>
+        <th scope="row">3</th>
+        <td colspan="2">Larry the Bird</td>
+        <td>Admin</td>
+        </tr>
+        <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>Cleint</td>
+        </tr>
+        <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>Cleint</td>
+        </tr>
+        <tr>
+        <th scope="row">3</th>
+        <td colspan="2">Larry the Bird</td>
+        <td>Admin</td>
+        </tr>
+        <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>Cleint</td>
+        </tr>
+        <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>Cleint</td>
+        </tr>
+        <tr>
+        <th scope="row">3</th>
+        <td colspan="2">Larry the Bird</td>
+        <td>Admin</td>
+        </tr>
+        <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>Cleint</td>
+        </tr>
+        <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>Cleint</td>
+        </tr>
+        <tr>
+        <th scope="row">3</th>
+        <td colspan="2">Larry the Bird</td>
+        <td>Admin</td>
+        </tr>
+        <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>Cleint</td>
+        </tr>
+        <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>Cleint</td>
+        </tr>
+        <tr>
+        <th scope="row">3</th>
+        <td colspan="2">Larry the Bird</td>
+        <td>Admin</td>
+        </tr>
+        
+    </tbody>
+    </table>
+</div>
+
+</section>
+
+@endsection
