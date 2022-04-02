70350d539716fad34d6f1edac405acd23d466b19
mohamedAyman <modyayman2001@gmail.com>
2022-04-03 01:37:01 +0200

    DecorationsPage-V1.0

---
+++ resources/views/decorationspage.blade.php
@@ -0,0 +1,264 @@
+@extends('layout')
+@section('title','Decoration Page')
+@section('content')
+
+<!-- slider_area_start
+    <div class="container">
+            <div class="row align-items-center">
+                <div class="col-xl-8 col-lg-8">
+                <div class="section_title">
+                            <h3>About the Company</h3>
+                        </div>
+                </div>
+                <div class="col-xl-8 col-lg-8">
+                    <div class="about_info">
+                        <p>This is some info about the company </p>
+                     
+                    </div>
+                </div>
+            </div>
+        </div>
+         -->
+    <div class="speakers_area inc_padd">
+        <div class="container">
+            <div class="doqrow">
+                <div class="column">
+                    <div class="single_speaker">
+                        <div class="speaker_thumb">
+                            <img src="img/speakers/1.png" alt="">
+                            <div class="hover_overlay">
+                                <div class="social_icon">
+                                    <a href="#">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="speaker_name text-center">
+                            <h3>Jonson Miller</h3>
+                            <p>Creative Director</p>
+                        </div>
+                    </div>
+                    <div class="single_speaker">
+                        <div class="speaker_thumb">
+                            <img src="img/speakers/3.png" alt="">
+                            <div class="hover_overlay">
+                                <div class="social_icon">
+                                <a href="#">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="speaker_name text-center">
+                            <h3>Albert Jackey</h3>
+                            <p>Product Designer</p>
+                        </div>
+                    </div>
+        
+
+                </div>
+                <div class="column">
+                    <div class="single_speaker">
+                        <div class="speaker_thumb">
+                            <img src="img/speakers/2.png" alt="">
+                            <div class="hover_overlay">
+                                <div class="social_icon">
+                                <a href="#">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="speaker_name text-center">
+                            <h3>Marked Macau</h3>
+                            <p>UI/UX Designer</p>
+                        </div>
+                    </div>
+                    <div class="single_speaker">
+                        <div class="speaker_thumb">
+                            <img src="img/speakers/1.png" alt="">
+                            <div class="hover_overlay">
+                                <div class="social_icon">
+                                <a href="#">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="speaker_name text-center">
+                            <h3>Kelvin Cooper</h3>
+                            <p>Art Director</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="single_speaker">
+                        <div class="speaker_thumb">
+                            <img src="img/speakers/1.png" alt="">
+                            <div class="hover_overlay">
+                                <div class="social_icon">
+                                <a href="#">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="speaker_name text-center">
+                            <h3>Jonson Miller</h3>
+                            <p>Creative Director</p>
+                        </div>
+                    </div>
+                    <div class="single_speaker">
+                        <div class="speaker_thumb">
+                            <img src="img/speakers/3.png" alt="">
+                            <div class="hover_overlay">
+                                <div class="social_icon">
+                                <a href="#">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="speaker_name text-center">
+                            <h3>Albert Jackey</h3>
+                            <p>Product Designer</p>
+                        </div>
+                    </div>
+        
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!-- speakers_end-->
+
+
+    <!-- resister_book_start -->
+    <!-- <div class="resister_book resister_bg_1">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="resister_text text-center">
+                        <h3>Register Now to Book <br>
+                            Your Presence</h3>
+                        <a href="#" class="boxed-btn-white">Book Now ($150) </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> -->
+    <!-- resister_book_end -->
+
+    <!-- brand_area_start -->
+    <!-- <div class="brand_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="brand_active owl-carousel">
+                        <div class="single_brand text-center">
+                            <img src="img/barnd/1.png" alt="">
+                        </div>
+                        <div class="single_brand text-center">
+                            <img src="img/barnd/2.png" alt="">
+                        </div>
+                        <div class="single_brand text-center">
+                            <img src="img/barnd/3.png" alt="">
+                        </div>
+                        <div class="single_brand text-center">
+                            <img src="img/barnd/4.png" alt="">
+                        </div>
+                        <div class="single_brand text-center">
+                            <img src="img/barnd/5.png" alt="">
+                        </div>
+                        <div class="single_brand text-center">
+                            <img src="img/barnd/6.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> -->
+    <!-- brand_area_end -->
+
+    <!-- faq_area_Start -->
+    <!--
+    <div class="faq_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="serction_title_large mb-95">
+                        <h3>
+                            FAQ
+                        </h3>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xl-12">
+                    <div id="accordion">
+                    <div class="card">
+                            <div class="card-header" id="headingOne">
+                                <h5 class="mb-0">
+                                    <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne"
+                                        aria-expanded="true" aria-controls="collapseOne">
+                                        <img src="img/barnd/info.png" alt="">Question1</span>
+                                    </button>
+                                </h5>
+                            </div>
+                            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne"
+                                data-parent="#accordion" style="">
+                                <div class="card-body">
+                                    Answer1
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-header" id="headingOne">
+                                <h5 class="mb-0">
+                                    <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne"
+                                        aria-expanded="true" aria-controls="collapseOne">
+                                        <img src="img/barnd/info.png" alt="">Question2</span>
+                                    </button>
+                                </h5>
+                            </div>
+                            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne"
+                                data-parent="#accordion" style="">
+                                <div class="card-body">
+                                    Answer2
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-header" id="headingOne">
+                                <h5 class="mb-0">
+                                    <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne"
+                                        aria-expanded="true" aria-controls="collapseOne">
+                                        <img src="img/barnd/info.png" alt="">Question3</span>
+                                    </button>
+                                </h5>
+                            </div>
+                            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne"
+                                data-parent="#accordion" style="">
+                                <div class="card-body">
+                                    Answer3
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-header" id="headingOne">
+                                <h5 class="mb-0">
+                                    <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne"
+                                        aria-expanded="true" aria-controls="collapseOne">
+                                        <img src="img/barnd/info.png" alt="">Question4</span>
+                                    </button>
+                                </h5>
+                            </div>
+                            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne"
+                                data-parent="#accordion" style="">
+                                <div class="card-body">
+                                    Answer4
+                                </div>
+                            </div>
+                        </div>
+                        
+                      
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+-->
+    <!-- faq_area_end -->
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::get('/productionHouse/editPackage',function(){return view('productionHous
 Route::get('/profile',function(){return view('profile');});
 Route::get('/editprofile',function(){return view('editprofile');});
 Route::get('/adminviewprofile',function(){return view('adminviewprofile');});
-
+Route::get('/decorationspage',function(){return view('decorationspage');});
 
 Route::get('/store',function(){
     $category = request('category');
