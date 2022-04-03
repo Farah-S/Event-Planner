7aede4ab5d618f8b7ed69bb871f6065c6ac25b59
Farah-S <farah.faroha132@gmail.com>
2022-04-04 00:46:28 +0200

    routes fixed

--- routes/web.php
+++ routes/web.php
@@ -15,23 +15,7 @@ use App\Http\Controllers\signupController;
 |
 */
 
-<<<<<<< HEAD
-Route::get('/login',[loginController::class,'index']);
-Route::get('/signup',[signupController::class,'index']);
-Route::get('/contact',[homeController::class,'contact'])->name('home.contact');
-Route::get('/about',[homeController::class,'about'])->name('home.about');
-Route::get('/cancelpolicy',[homeController::class,'about'])->name('home.cancelpolicy');
-Route::get('/',[homeController::class,'index']);
-Route::get('/events',function(){return view('events/events');})->name('events.events');
-Route::get('/productionHouse',function(){return view('productionHouse/productionhouse');})->name('productionHouse.productionHouse');
-Route::get('/productionHouse/editPackage',function(){return view('productionHouse/editPackage');});
-Route::get('/profile',function(){return view('profile');});
-Route::get('/editprofile',function(){return view('editprofile');});
-Route::get('/adminviewprofile',function(){return view('adminviewprofile');});
-Route::get('/viewallusers',function(){return view('viewallusers');});
-=======
 Route::get('/login',[loginController::class,'index'])->name('user.login');
->>>>>>> 666fc9b15008653925c3459c0eb1243c9fefa9ef
 
 Route::get('signup',[signupController::class,'index'])->name('user.signup');
 
@@ -56,13 +40,5 @@ Route::get('/editprofile',function(){return view('editprofile');});
 Route::get('/packagedetails',function(){return view('packagedetails');});
 
 Route::get('/admin/viewprofile',function(){return view('adminviewprofile');});
-Route::get('/productionHouse',function(){return view('decorationspage');})->name('productionHouse.packages');
-
-Route::get('/store',function(){
-    $category = request('category');
-
-    if (isset($category)){
-    return ' you are veiwing the store for '. strip_tags($category);
-}
-    return ' you are veiwing all instruments ';
-});
+Route::get('/productionHouse',function(){return view('productionHouse/decorationspage');})->name('productionHouse.packages');
+Route::get('/viewallusers',function(){return view('viewallusers');});
