d4932e5dfb9918cdb37c726d0bd0158ac3fe64d4
Farah-S <farah.faroha132@gmail.com>
2022-04-03 23:34:55 +0200

    updated routes

--- routes/web.php
+++ routes/web.php
@@ -29,19 +29,19 @@ Route::get('/',[homeController::class,'index']);
 
 Route::get('/events',function(){return view('events/events');})->name('events.events');
 
-Route::get('/productionHouse',function(){return view('productionHouse/productionhouse');})->name('productionHouse.packages');
-
 Route::get('/productionHouse/editPackage',function(){return view('productionHouse/editPackage');});
 
-Route::get('/profile',function(){return view('profile');})->name('user.profile');
+Route::get('/myprofile',function(){return view('profile');})->name('user.profile');
 
 Route::get('/myorders',function(){return view('customers/myorders');})->name('user.myorders');
 
 Route::get('/editprofile',function(){return view('editprofile');});
 
-Route::get('/adminviewprofile',function(){return view('adminviewprofile');});
-Route::get('/decorationspage',function(){return view('decorationspage');});
 Route::get('/packagedetails',function(){return view('packagedetails');});
+
+Route::get('/admin/viewprofile',function(){return view('adminviewprofile');});
+Route::get('/productionHouse',function(){return view('decorationspage');})->name('productionHouse.packages');
+
 Route::get('/store',function(){
     $category = request('category');
 
