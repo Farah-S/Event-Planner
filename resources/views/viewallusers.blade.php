421a5c645c78a8d5f58d79c4384fdadfadc0819a
ahmad123ezpz <ahmed.amr.mmm@gmail.com>
2022-04-04 22:38:55 +0200

    Users list page

--- resources/views/viewallusers.blade.php
+++ resources/views/viewallusers.blade.php
@@ -2,16 +2,32 @@
 @section('title','View Users')
 @section('content')
 <section class="contact-section">
-<div class="slider_area bg-test-img">
+<!-- <div class="slider_area bg-test-img"> -->
     <div class="row justify-content-center">
-                        <div class="col-12">
+                        <div class="col-12 ">
                             <h3 class=" slider slider2 align-mid">Users Table</h3>
                         </div>
-                    </div>
-                    
+                    <!-- </div> -->
 <div class="container">
+<input class="form-control" id="searchbar" type="text" placeholder="Search..">
+<div class="row">
+<div class="col-md-12 text-right">
+  <!-- <div class="dropdown">
+    <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">User Type -->
+    <!--<span class="caret"></span>--></button>
+    <!-- <ul class="dropdown-menu">
+      <li>Admin</li>
+      <li>Cleint</li>
+    </ul>
+  </div> -->
+</div>
+</div>
+</div>
+<div class="container">
+<div class="row">
+<div class="col-md-6">
     <table class="table table-bordered table-dark table-hover">
-    <caption>List of users</caption>
+    <caption>List of Client</caption>
     <thead>
         <tr>
         <th scope="col">#</th>
@@ -25,13 +41,115 @@
         <th scope="row">1</th>
         <td>Mark</td>
         <td>Otto</td>
-        <td>Cleint</td>
+        <td>Client</td>
         </tr>
         <tr>
         <th scope="row">2</th>
         <td>Jacob</td>
         <td>Thornton</td>
-        <td>Cleint</td>
+        <td>Client</td>
+        </tr>
+        <tr>
+        <th scope="row">3</th>
+        <td colspan="2">Larry the Bird</td>
+        <td>Client</td>
+        </tr>
+        <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>Client</td>
+        </tr>
+        <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>Client</td>
+        </tr>
+        <tr>
+        <th scope="row">3</th>
+        <td colspan="2">Larry the Bird</td>
+        <td>Client</td>
+        </tr>
+        <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>Client</td>
+        </tr>
+        <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>Client</td>
+        </tr>
+        <tr>
+        <th scope="row">3</th>
+        <td colspan="2">Larry the Bird</td>
+        <td>Client</td>
+        </tr>
+        <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>Client</td>
+        </tr>
+        <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>Client</td>
+        </tr>
+        <tr>
+        <th scope="row">3</th>
+        <td colspan="2">Larry the Bird</td>
+        <td>Client</td>
+        </tr>
+        <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>Client</td>
+        </tr>
+        <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>Client</td>
+        </tr>
+        <tr>
+        <th scope="row">3</th>
+        <td colspan="2">Larry the Bird</td>
+        <td>Client</td>
+        </tr>
+        
+    </tbody>
+    </table>
+</div>
+<!--the other table's section-->
+<div class="col-md-6">
+    <table class="table table-bordered table-dark table-hover">
+    <caption>List of Admins</caption>
+    <thead>
+        <tr>
+        <th scope="col">#</th>
+        <th scope="col">First Name</th>
+        <th scope="col">Last Name</th>
+        <th scope="col">User Type</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>Admin</td>
+        </tr>
+        <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>Admin</td>
         </tr>
         <tr>
         <th scope="row">3</th>
@@ -42,13 +160,13 @@
         <th scope="row">1</th>
         <td>Mark</td>
         <td>Otto</td>
-        <td>Cleint</td>
+        <td>Admin</td>
         </tr>
         <tr>
         <th scope="row">2</th>
         <td>Jacob</td>
         <td>Thornton</td>
-        <td>Cleint</td>
+        <td>Admin</td>
         </tr>
         <tr>
         <th scope="row">3</th>
@@ -59,13 +177,13 @@
         <th scope="row">1</th>
         <td>Mark</td>
         <td>Otto</td>
-        <td>Cleint</td>
+        <td>Admin</td>
         </tr>
         <tr>
         <th scope="row">2</th>
         <td>Jacob</td>
         <td>Thornton</td>
-        <td>Cleint</td>
+        <td>Admin</td>
         </tr>
         <tr>
         <th scope="row">3</th>
@@ -76,13 +194,13 @@
         <th scope="row">1</th>
         <td>Mark</td>
         <td>Otto</td>
-        <td>Cleint</td>
+        <td>Admin</td>
         </tr>
         <tr>
         <th scope="row">2</th>
         <td>Jacob</td>
         <td>Thornton</td>
-        <td>Cleint</td>
+        <td>Admin</td>
         </tr>
         <tr>
         <th scope="row">3</th>
@@ -93,13 +211,13 @@
         <th scope="row">1</th>
         <td>Mark</td>
         <td>Otto</td>
-        <td>Cleint</td>
+        <td>Admin</td>
         </tr>
         <tr>
         <th scope="row">2</th>
         <td>Jacob</td>
         <td>Thornton</td>
-        <td>Cleint</td>
+        <td>Admin</td>
         </tr>
         <tr>
         <th scope="row">3</th>
@@ -109,6 +227,8 @@
         
     </tbody>
     </table>
+</div>
+</div>
 </div>
 </div>
 
