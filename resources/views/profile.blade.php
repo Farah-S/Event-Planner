5a4e48c0343c96c2d7287b03310b0e3424c3b298
IbrahimBaher <ibrahim1909541@miuegypt.edu.eg>
2022-04-05 01:30:05 +0200

    profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -4,48 +4,43 @@
 
 <div class="slider_area">
     </div>
-    <!-- slider_area_start -->
-    <div class="ibspeakers_area">
-        <div class="container ibcontainer">
-            <div class="row">
-                <div class="col-xl-5 col-md-6">
-                    <div class="single_speaker">
-                        <div class="speaker_thumb">
-                            <img src="img/speakers/1.png" alt="">
-                        </div>
-                    <div class="about_info">
+<div class="ibcontainer">
+                            <img src="img/speakers/1.png">
+                      
+                        <div style = "position:relative; left:600px; bottom:450px;">
+                        <div class="about_info">
                         <div class="section_title">
                             <h3 class="slider">Name</h3>
                             <p class="himasfont">Ibrahim Baher</p>
+                        <br><br>
+                        
+                  
+                       
                             <h3 class="slider">Email</h3>
                             <p class="himasfont">Ibrahim.baher@email.com</p>
                         </div>
-                    </div>
-                    <br>
-                 </div>
-                     </div>
-                     <div class="col-lg-4">
-                            <div class="row justify-content-center ">
+                       
+      </div>
+                        </div>
+                        <div style = "position:relative; left:0px; bottom:150px;">
                                 
                                 <button type="submit" onclick="location.href = '{{route('user.editProfile')}}';" class="button button-contactForm boxed-btn">Edit profile</button>
-                            </div>
-                            <br>
-                            <div class="row justify-content-center ">
+                                </div>
+                            
+                                <div style = "position:relative; left:185px; bottom:210px;">
                                 
                                 <button type="submit" class="button button-contactForm boxed-btn">Delete Profile</button>
                             
-                            </div><br>
-                            <div class="row justify-content-center ">
-                                
+                                </div>
+                           
+                                <div style = "position:relative; left:385px; bottom:270px;">
                                 <button type="submit" onclick="location.href = '{{route('customer.myorders')}}';" class="button button-contactForm boxed-btn">Show Orders</button>
+                                </div>
+                           
                             
-                            </div>
-                            </div>
-                            
-                            
-            </div>    
-        </div>  
-    </div>  
+    </div>
+    
+
                 
   
 
