d6479d50e97f17cd435660626e969817133da8be
IbrahimBaher <ibrahim1909541@miuegypt.edu.eg>
2022-05-31 13:41:48 +0200

    removing notes

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -21,28 +21,7 @@
             </div>
         </div>
         </div>
-        <!-- <div class="countDOwn_area">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-xl-4 col-md-6 col-lg-4">
-                        <div class="single_date">
-                            <i class="ti-location-pin"></i>
-                            <span>City Hall, New York City</span>
-                        </div>
-                    </div>
-                    <div class="col-xl-3 col-md-6 col-lg-3">
-                        <div class="single_date">
-                            <i class="ti-alarm-clock"></i>
-                            <span>12-15 Sep 2019</span>
-                        </div>
-                    </div>
-                    <div class="col-xl-5 col-md-12 col-lg-5">
-                        <span id="clock"></span>
-                    </div>
-
-                </div>
-            </div>
-        </div> -->
+      
     </div>
     <!-- slider_area_end -->
 
@@ -79,7 +58,7 @@
                         </div>
                         <div class="speaker_name text-center">
                             <h3>Jonson Miller</h3>
-                            <p>Ragel Gada3 we 3am 2el nas Ragel Gada3 we 3am 2el nas Ragel Gada3 we 3am 2el nas Ragel Gada3 we 3am 2el nas Ragel Gada3 we 3am 2el nas Ragel Gada3 we 3am 2el nas Ragel Gada3 we 3am 2el nas</p>
+                            <p>team member info</p>
                         </div>
                     </div>
                     <div class="single_speaker">
@@ -95,27 +74,12 @@
                         </div>
                         <div class="speaker_name text-center">
                             <h3>Albert Jackey</h3>
-                            <p>Product Designer</p>
+                            <p>Product Designer + info</p>
                         </div>
                     </div>
                 </div>
                 <div class="col-xl-5 offset-xl-2 col-md-6">
-                    <!-- <div class="single_speaker">
-                        <div class="speaker_thumb">
-                            <img src="img/speakers/2.png" alt="">
-                            <div class="hover_overlay">
-                                <div class="social_icon">
-                                    <a href="#"><i class="fa fa-facebook"></i></a>
-                                    <a href="#"><i class="fa fa-instagram"></i></a>
-                                    <a href="#"><i class="fa fa-twitter"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="speaker_name text-center">
-                            <h3>Marked Macau</h3>
-                            <p>UI/UX Designer</p>
-                        </div>
-                    </div> -->
+                 
                     <div class="single_speaker">
                         <div class="speaker_thumb">
                             <img src="img/speakers/1.png" alt="">
@@ -136,55 +100,6 @@
             </div>
         </div>
     </div>
-    <!-- speakers_end-->
-
-
-    <!-- resister_book_start -->
-    <!-- <div class="resister_book resister_bg_1">
-        <div class="container">
-            <div class="row">
-                <div class="col-xl-12">
-                    <div class="resister_text text-center">
-                        <h3>Register Now to Book <br>
-                            Your Presence</h3>
-                        <a href="#" class="boxed-btn-white">Book Now ($150) </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> -->
-    <!-- resister_book_end -->
-
-    <!-- brand_area_start -->
-    <!-- <div class="brand_area">
-        <div class="container">
-            <div class="row">
-                <div class="col-xl-12">
-                    <div class="brand_active owl-carousel">
-                        <div class="single_brand text-center">
-                            <img src="img/barnd/1.png" alt="">
-                        </div>
-                        <div class="single_brand text-center">
-                            <img src="img/barnd/2.png" alt="">
-                        </div>
-                        <div class="single_brand text-center">
-                            <img src="img/barnd/3.png" alt="">
-                        </div>
-                        <div class="single_brand text-center">
-                            <img src="img/barnd/4.png" alt="">
-                        </div>
-                        <div class="single_brand text-center">
-                            <img src="img/barnd/5.png" alt="">
-                        </div>
-                        <div class="single_brand text-center">
-                            <img src="img/barnd/6.png" alt="">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> -->
-    <!-- brand_area_end -->
 
     <!-- faq_area_Start -->
     <div class="faq_area">
